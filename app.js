2181123d84bdb05df73a28f3a61684b87fe2babd
Ethan Sherbondy <sherbondye@gmail.com>
2018-07-21 03:43:20 -0400

    cost estimates...

--- app.js
+++ app.js
@@ -14,20 +14,24 @@ var modelGroups = {};
 // mapping from element id to bbox size in raw unit (need to convert to ft)...
 var elementSizes = {};
 
+var subgroupTotals = {};
+
+var groupCosts = {};
+
 // dimensions to keep for square footage measurements...
 var dimensionsToKeep = {
     'Basic': {y: true, z: true},
     'Floor': {x: true, y: true},
-    'Door-Passage-Single-Two': {y: true, z: true},
-    'Door-Double-Flush': {y: true, z: true},
-    'Fixed': {y: true, z: true},
-    'Desk': {x: true, y: true},
-    'Chair-Task': {x: true, y: true, z: true},
-    'Table-Dining': {x: true, y: true},
-    'Chair-Breuer': {x: true, y: true},
+    'Door-Passage-Single-Two': {y: true, z: true, unit: true},
+    'Door-Double-Flush': {y: true, z: true, unit: true},
+    'Fixed': {y: true, z: true, unit: true},
+    'Desk': {x: true, y: true, unit: true},
+    'Chair-Task': {x: true, y: true, z: true, unit: true},
+    'Table-Dining': {x: true, y: true, unit: true},
+    'Chair-Breuer': {x: true, y: true, unit: true},
     'Compound': {x: true, y: true},
-    'Door-Exterior-Double-Two': {x: true, z: true},
-    'Counter': {x: true, y: true},
+    'Door-Exterior-Double-Two': {x: true, z: true, unit: true},
+    'Counter': {x: true, y: true, unit: true},
     'System': {y: true, z: true},
     'Rectangular': {x: true, z: true},
     // here be dragons...
@@ -36,6 +40,34 @@ var dimensionsToKeep = {
     'Railing': {x: true, z: true},
     'Top': {x: true, z: true}
 };
+
+
+var materialCosts = {
+    // sq ft...
+    'Basic_Wall_Exterior_-_Dark_Brick_on_CMU': 80,
+    'Basic_Roof_Generic_-_12"': 10,
+    'Basic_Wall_Interior_-_5_12"_Partition_(1-hr)': 26,
+    'Basic_Wall_Interior_-_6_18"_Partition_(2-hr)': 26,
+    'Basic_Wall_Interior_-_5"_Partition_(2-hr)': 26,
+    'Floor_Generic_-_12"': 20,
+    "Compound_Ceiling_2'_x_4'_ACT_System": 23,
+    "Compound_Ceiling_1'_x_4'_ACT_Ceiling": 23,
+    "Compound_Ceiling_2'_x_2'_ACT_System": 23,
+    "Compound_Ceiling_GWB_on_Mtl_Stud": 23,
+    "System_Panel_Glazed": 80,
+    'Rectangular_Mullion_25"_x_5"_rectangular ': 2,
+    // per unit...
+    'Door-Passage-Single-Two_Lite_Narrow_42"_x_94"': 100,
+    'Door-Double-Flush_Panel_40"_x_84"': 100,
+    'Fixed_36"_x_96"': 100,
+    'Desk_72"_x_36"': 80,
+    'Chair-Task_Arms_Chair-Task_Arms': 80,
+    'Table-Dining_Round_w_Chairs_36"_Diameter': 120,
+    'Chair-Breuer_Chair-Breuer': 40,
+    'Door-Exterior-Double-Two_Lite_96"_x_84"': 200,
+    'Counter_Top_24"_Depth': 100,
+};
+
 
 const defaultMaterial = new THREE.MeshPhongMaterial(
     {
@@ -138,8 +170,48 @@ function main() {
 
                 const bbox = new THREE.Box3().setFromObject(child);
                 elementSizes[child.id] = bbox.getSize();
+
+
+                const subgroupName = getSubgroupName(child);
+
+                let childSize = elementSizes[child.id];
+                let squareFootage = getSquareFootage(namePrefix, childSize);
+
+                if (subgroupName in subgroupTotals) {
+                    subgroupTotals[subgroupName]['count'] += 1;
+                    subgroupTotals[subgroupName]['totalSquareFootage'] += squareFootage;
+                } else {
+                    subgroupTotals[subgroupName] = {
+                        'count': 1,
+                        'totalSquareFootage': squareFootage,
+                        'cost': 0
+                    }
+                }
             }
         });
+
+        for (const subgroupName in subgroupTotals) {
+            const namePrefix = subgroupName.split('_')[0];
+            const baseCost = materialCosts[subgroupName] || 0;
+
+            const useUnitCost = dimensionsToKeep[namePrefix]["unit"] === true;
+            var cost = 0;
+
+            if (useUnitCost) {
+                cost = subgroupTotals[subgroupName]['count'] * baseCost;
+            } else {
+                cost = subgroupTotals[subgroupName]['totalSquareFootage'] * baseCost;
+            }
+
+            subgroupTotals[subgroupName]['cost'] = cost;
+
+            if (namePrefix in groupCosts) {
+                groupCosts[namePrefix] += cost;
+            } else {
+                groupCosts[namePrefix] = cost;
+            }
+        }
+
 
         for (const groupName in modelGroups) {
             modelChildrenList.append(
@@ -152,7 +224,6 @@ function main() {
 
         scene.add(model);
         objs.push(model);
-
     });
 
     // animation rendering
@@ -176,6 +247,25 @@ const objs = main();
 
 function coordFloat(coord){
     return parseFloat(coord.childNodes[0].nodeValue);
+}
+
+
+function getSubgroupName(child) {
+    const splitName = child.name.split('_');
+    const nameWithoutID = splitName.slice(0, -1);
+    return nameWithoutID.join('_');
+}
+
+function getSquareFootage(group, childSize) {
+    var squareFootage = 1;
+    const areaDimensions = dimensionsToKeep[group];
+
+    for (const dimension in areaDimensions) {
+        if (["x", "y", "z"].includes(dimension)) {
+            squareFootage *= childSize[dimension];
+        }
+    }
+    return squareFootage;
 }
 
 
@@ -214,8 +304,9 @@ $(function(){
 
                const startingVertex = vertexCount;
 
+               // @TODO: fix for closed shells with faces made of something other than than 4 points...
                for (var k = 0; k < points.length; k+=1) {
-                   console.log(points.length);
+                   // console.log(points.length);
                    const coords = points[k].getElementsByTagName("Coordinate");
                    const x = coordFloat(coords[0]);
                    const y = coordFloat(coords[1]);
@@ -227,7 +318,7 @@ $(function(){
                }
 
                // (A, B, C), (A, C, D)
-               
+
                const faceA = new THREE.Face3(vertexCount - 4, vertexCount - 3, vertexCount - 2);
                const faceB = new THREE.Face3(vertexCount - 4, vertexCount - 2, vertexCount - 1);
 
@@ -269,10 +360,7 @@ $(function(){
 
        childElementsList.html("");
 
-       var totalSize = new THREE.Vector3(0,0,0);
-       var totalSquareFootage = 0;
-
-       var subgroupTotals = {};
+       var activeSubgroups = {};
 
        sceneModel.children.forEach(function(child) {
            if (child.isMesh) {
@@ -280,32 +368,12 @@ $(function(){
                    child.material = highlightMaterial;
 
                    // const center = child.geometry.boundingSphere.center;
+                   const subgroupName = getSubgroupName(child);
 
-                   var splitName = child.name.split('_');
-                   var nameWithoutID = splitName.slice(0, -1);
-                   var subgroupName = nameWithoutID.join('_');
+                   activeSubgroups[subgroupName] = true;
 
-
-                   var childSize = elementSizes[child.id];
-
-                   var squareFootage = 1;
-                   var areaDimensions = dimensionsToKeep[activeGroup];
-                   for (const dimension in areaDimensions) {
-                       squareFootage *= childSize[dimension];
-                   }
-
-
-                   if (subgroupName in subgroupTotals) {
-                       subgroupTotals[subgroupName]['count'] += 1;
-                       subgroupTotals[subgroupName]['totalSquareFootage'] += squareFootage;
-                   } else {
-                       subgroupTotals[subgroupName] = {
-                           'count': 1,
-                           'totalSquareFootage': squareFootage
-                       }
-                   }
-
-
+                   let childSize = elementSizes[child.id];
+                   let squareFootage = getSquareFootage(activeGroup, childSize);
                    var squareFootageNice = parseInt(squareFootage);
 
                    childElementsList.append(
@@ -314,10 +382,6 @@ $(function(){
                             ${child.id}: ${child.name} ( ${squareFootageNice} ft^2 )
                         </a>`
                    );
-
-                   totalSize.add(childSize);
-
-                   totalSquareFootage += squareFootage;
 
                } else {
                    child.material = defaultMaterial;
@@ -329,15 +393,17 @@ $(function(){
 
        $("#cost-summary").html("");
 
-       for (const subgroupName in subgroupTotals) {
+       for (const subgroupName in activeSubgroups) {
            const totals = subgroupTotals[subgroupName];
            const niceSquareFootage = parseInt(totals.totalSquareFootage);
+           const totalCost = parseInt(totals.cost);
 
            $("#cost-summary").append(
                `<div class="card">
                     <div class="card-body">
                         <h4 class="card-title">${subgroupName} Total Measurements</h4>
                         
+                        <p><strong>Total Cost</strong>: $${totalCost}</p>
                         <p><strong>Count</strong>: ${totals.count}</p>
                         <p><strong>Square Footage Total</strong>: ${niceSquareFootage} ft^2</p>
                     </div>
