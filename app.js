298b9d75aa8bf9a16886b7334f6752594b7c7b74
Ethan Sherbondy <sherbondye@gmail.com>
2018-07-20 21:30:33 -0400

    rendering school house model...

--- app.js
+++ app.js
@@ -2,6 +2,21 @@
 
 /*globals THREE */
 
+function addSpotLight(scene) {
+    var spotLight = new THREE.SpotLight( 0xffffff );
+    spotLight.position.set( 100, 1000, 100 );
+
+    spotLight.castShadow = true;
+
+    spotLight.shadow.mapSize.width = 1024;
+    spotLight.shadow.mapSize.height = 1024;
+
+    spotLight.shadow.camera.near = 500;
+    spotLight.shadow.camera.far = 4000;
+    spotLight.shadow.camera.fov = 30;
+    scene.add(spotLight);
+}
+
 // simplified on three.js/examples/webgl_loader_fbx.html
 function main() {
     // renderer
@@ -12,35 +27,58 @@ function main() {
     container.appendChild(renderer.domElement);
 
     // camera
-    const camera = new THREE.PerspectiveCamera(30, 800 / 600, 1, 10000);
-    camera.position.set(0, 10, 100);
+    window.camera = new THREE.PerspectiveCamera(30, 800 / 600, 1, 10000);
+
+    const controls = new THREE.OrbitControls( camera );
+
+    camera.position.set(0, -250, 250);
     camera.up.set(0, 1, 0);
-    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    camera.lookAt(new THREE.Vector3(20, 20, 100));
+    controls.update();
 
     // scene and lights
     const scene = new THREE.Scene();
-    scene.add(new THREE.AmbientLight(0xcccccc));
+    scene.add(new THREE.AmbientLight(0xcccccc, 0.5));
+
+    const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.75 );
+    directionalLight.position.set( new THREE.Vector3(100, 1000, 100) );
+    // scene.add(directionalLight);
+
+    addSpotLight(scene);
+
+    // debug box
+    const box = new THREE.Box3();
+    box.setFromCenterAndSize( new THREE.Vector3( 1, 1, 1 ), new THREE.Vector3( 2, 1, 3 ) );
+
+    const helper = new THREE.Box3Helper( box, 0xffff00 );
+    scene.add( helper );
 
     // load fbx model and texture
     const objs = [];
     const loader = new THREE.FBXLoader();
-    loader.load("./xsi_man_skinning.fbx", model => {
-        // model is a THREE.Group (THREE.Object3D)
-        const mixer = new THREE.AnimationMixer(model);
-        // animations is a list of THREE.AnimationClip
-        mixer.clipAction(model.animations[0]).play();
+
+    const phongMaterial = new THREE.MeshPhongMaterial(
+        { ambient: 0x555555, color: 0x555555, specular: 0xffffff, shininess: 50, shading: THREE.SmoothShading }
+    );
+
+    loader.load("./schoolhouse.fbx", model => {
+        model.material = phongMaterial;
         scene.add(model);
-        objs.push({model, mixer});
+        objs.push(model);
+        directionalLight.target = model;
     });
 
     // animation rendering
     const clock = new THREE.Clock();
+
     (function animate() {
-        // animation with THREE.AnimationMixer.update(timedelta)
-        objs.forEach(({mixer}) => {mixer.update(clock.getDelta());});
+        controls.update();
+
         renderer.render(scene, camera);
+
         requestAnimationFrame(animate);
     })();
+
     return objs;
 }
 const objs = main();
